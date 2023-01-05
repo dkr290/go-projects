46ca8657b54b1d4f7534f512b365205a688fa8a0
Danail Surudzhiyski <dans@bankingcircle.com>
2023-01-05 17:55:13 +0100

    commit changes to the pipeline and new app

---
+++ bookstore-app/bookstore/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import "github.com/gorilla/mux"
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book{bookID}", controllers.GetBookByID).Methods("GET")
+	router.HandleFunc("/book{bookID}", controllers.UpdateBookByID).Methods("PUT")
+	router.HandleFunc("/book{bookID}", controllers.DeleteBookByID).Methods("DELETE")
+
+}
