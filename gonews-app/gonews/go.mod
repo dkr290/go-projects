52c42a07902213c46b29b95e20ec2ce4b3ea04f6
Danail Surudzhiyski <dans@bankingcircle.com>
2023-01-17 17:20:53 +0100

    adding migrations and postgresql yaml files

---
+++ gonews-app/gonews/go.mod
@@ -0,0 +1,5 @@
+module github.com/dkr290/go-projects/gonews
+
+go 1.19
+
+require github.com/google/uuid v1.3.0
---
+++ gonews-app/gonews/migrations/1_create_tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE threads(
+    id UUID PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+
+);
+
+
+CREATE TABLE posts(
+    id UUID PRIMARY KEY,
+    thread_id UUID NOT NULL REFERENCES threads (id) IN DELETE CASCADE,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    votes INT DEFAULT 0
+);
+
+
+CREATE TABLE comments(
+    id UUID PRIMARY KEY,
+    post_id UUID NOT NULL REFERENCES posts (id) IN DELETE CASCADE,
+    content TEXT NOT NULL,
+    votes INT DEFAULT 0
+);
