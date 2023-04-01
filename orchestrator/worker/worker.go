7acf7bee11a43a5cd33f82b92bc21c1e988f9814
dkrasimirov <dkrasimirov@example.com>
2023-04-02 00:18:06 +0200

    orchestrator project

---
+++ orchestrator/worker/worker.go
@@ -0,0 +1,24 @@
+package worker
+
+import (
+	"fmt"
+
+	"github.com/dkr290/go-projects/orchestrator/task"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
