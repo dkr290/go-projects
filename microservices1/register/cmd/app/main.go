9ea1ea1549795932ab2ed28ef6f2900b96e882b2
dkrasimirov <dkrasimirov@example.com>
2023-05-14 14:33:51 +0200

    adding db connect

--- microservices1/register/cmd/app/main.go
+++ microservices1/register/cmd/app/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"register/data"
+	"time"
 )
 
 const webPort = "80"
+
+var counts int64
 
 type Config struct {
 	Models data.Models
@@ -17,6 +22,13 @@ func main() {
 
 	app := Config{}
 	log.Printf("Starting register service at port %s", webPort)
+
+	//connect to Database
+
+	conn := connectToDb()
+	if conn == nil {
+		log.Panic("Can't connect to the postgres")
+	}
 
 	// going to the chi router in routes and then to each handler
 	srv := &http.Server{
@@ -30,4 +42,44 @@ func main() {
 		log.Panic(err)
 	}
 
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
+
+func connectToDb() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgress server is not yet ready")
+			counts++
+		} else {
+			log.Println("Connected to postgres")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds...")
+		time.Sleep(2 * time.Second)
+		continue
+
+	}
 }
