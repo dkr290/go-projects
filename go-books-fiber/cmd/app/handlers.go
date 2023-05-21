9ec96267e9ff16fec4d3e55c98875cee50567d43
dkrasimirov <dkrasimirov@example.com>
2023-05-21 14:47:10 +0200

    adding project all functions

--- go-books-fiber/cmd/app/handlers.go
+++ go-books-fiber/cmd/app/handlers.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/dkr290/go-projects/go-fiber-postgres/models"
+	"github.com/dkr290/go-projects/go-books-fiber/models"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -32,6 +33,32 @@ func (c *Config) CreateBook(context *fiber.Ctx) error {
 }
 
 func (c *Config) DeleteBook(context *fiber.Ctx) error {
+	books := models.Books{}
+	id := context.Params("id")
+
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message": "id cannot be empty",
+		})
+		return nil
+	}
+
+	err := c.DB.Delete(&books, id)
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "coud not delete book"})
+		return err.Error
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "book deleted successfully",
+	})
+
+	return nil
+
+}
+
+func (c *Config) GetBooks(context *fiber.Ctx) error {
 	books := []models.Books{}
 	err := c.DB.Find(&books).Error
 	if err != nil {
@@ -46,9 +73,30 @@ func (c *Config) DeleteBook(context *fiber.Ctx) error {
 	})
 
 	return nil
-
 }
 
-func (c *Config) GetBooks(context *fiber.Ctx) error {
+func (c *Config) GetBookById(context *fiber.Ctx) error {
 
+	id := context.Params("id")
+	book := &models.Books{}
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
+			"message": "id cannot be empty",
+		})
+		return nil
+	}
+
+	fmt.Println("the ID is ", id)
+	if err := c.DB.Where("id = ?", id).First(&book).Error; err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get the book"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "book id found sucessfully",
+		"data":    book,
+	})
+
+	return nil
 }
