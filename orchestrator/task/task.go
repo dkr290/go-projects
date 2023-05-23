4d79f524da4f906508356dd71f45577e2b1c630c
dkr290 <39731809+dkr290@users.noreply.github.com>
2023-05-23 15:58:55 +0000

    adding skeleton

--- orchestrator/task/task.go
+++ orchestrator/task/task.go
@@ -1,16 +1,8 @@
 package task
 
 import (
-	"context"
-	"io"
-	"log"
-	"os"
 	"time"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
 )
@@ -27,7 +19,6 @@ const (
 
 type Task struct {
 	ID            uuid.UUID
-	ContainerID   string
 	Name          string
 	State         State
 	Image         string
@@ -41,121 +32,8 @@ type Task struct {
 }
 
 type TaskEvent struct {
-	ID       uuid.UUID
-	State    State
-	TimStamp time.Time
-	Task     Task
-}
-
-type Docker struct {
-	Client      *client.Client
-	Config      Config
-	ContainerId string
-}
-
-func NewDocker() *Docker {
-	return &Docker{}
-}
-
-func NewConifg(c *Task) *Task {
-	return c
-}
-
-type DockerResult struct {
-	Error       error
-	Action      string
-	ContainerId string
-	Result      string
-}
-
-func (d *Docker) Run() DockerResult {
-
-	ctx := context.Background()
-	reader, err := d.Client.ImagePull(
-		ctx,
-		d.Config.Image,
-		types.ImagePullOptions{},
-	)
-	if err != nil {
-		log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
-		return DockerResult{Error: err}
-	}
-	io.Copy(os.Stdout, reader)
-
-	// Create Container with below
-	rp := container.RestartPolicy{
-		Name: d.Config.RestartPolicy,
-	}
-	r := container.Resources{
-		Memory: d.Config.Memory,
-	}
-	cc := container.Config{
-		Image: d.Config.Image,
-		Env:   d.Config.Env,
-	}
-	hc := container.HostConfig{
-		RestartPolicy:   rp,
-		Resources:       r,
-		PublishAllPorts: true,
-	}
-
-	resp, err := d.Client.ContainerCreate(ctx, &cc, &hc, nil, nil, d.Config.Name)
-
-	if err != nil {
-		log.Printf("Error creating container using the image %s: %v\n", d.Config.Image, err)
-	}
-	err = d.Client.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
-	if err != nil {
-		log.Printf("Errorr starting container %s: %v\n", resp.ID, err)
-		return DockerResult{Error: err}
-	}
-
-	d.ContainerId = resp.ID
-
-	out, err := d.Client.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{
-		ShowStdout: true,
-		ShowStderr: true,
-	})
-
-	if err != nil {
-		log.Printf("Error getting logs for container %s: %v\n", resp.ID, err)
-		return DockerResult{Error: err}
-	}
-
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
-	//return
-	return DockerResult{
-		ContainerId: resp.ID,
-		Action:      "start",
-		Result:      "success",
-	}
-}
-
-func (d *Docker) Stop(id string) DockerResult {
-
-	log.Printf("Attempting to stop the container %s", id)
-	ctx := context.Background()
-	opts := container.StopOptions{
-		Signal: "SIGTERM",
-	}
-	err := d.Client.ContainerStop(ctx, id, opts)
-	if err != nil {
-		log.Printf("Unable to stop container %s: %s", id, err)
-
-	}
-	removeOptions := types.ContainerRemoveOptions{
-		RemoveVolumes: true,
-		Force:         true,
-	}
-
-	if err := d.Client.ContainerRemove(ctx, id, removeOptions); err != nil {
-		log.Printf("Unable to remove container: %s", err)
-
-	}
-
-	return DockerResult{
-		Action: "stop",
-		Result: "success",
-		Error:  nil,
-	}
+	ID        uuid.UUID
+	State     State
+	Timestamp time.Time
+	Task      Task
 }
--- orchestrator/worker/worker.go
+++ orchestrator/worker/worker.go
@@ -2,8 +2,6 @@ package worker
 
 import (
 	"fmt"
-	"log"
-	"time"
 
 	"github.com/dkr290/go-projects/orchestrator/task"
 	"github.com/golang-collections/collections/queue"
@@ -17,49 +15,18 @@ type Worker struct {
 	TaskCount int
 }
 
-func (w *Worker) CollectStats() {
-	fmt.Println("I will collect stats")
-}
-
 func (w *Worker) RunTask() {
-	fmt.Println("I will start or stop a task")
+	fmt.Println("Running the task")
 }
 
-func (w *Worker) StartTask(t task.Task) task.DockerResult {
-	t.StartTime = time.Now().UTC()
-
-	d := task.NewDocker()
-	result := d.Run()
-
-	if result.Error != nil {
-		log.Printf("Error running task %v: %v\n", t.ID, result.Error)
-		t.State = task.Failed
-		w.Db[t.ID] = &t
-		return result
-
-	}
-
-	t.ContainerID = result.ContainerId
-	t.State = task.Running
-	w.Db[t.ID] = &t
-
-	return result
-
+func (w *Worker) CollectStats() {
+	fmt.Println("Collection the stats")
 }
 
-func (w *Worker) StopTask(t task.Task) task.DockerResult {
+func (w *Worker) StartTask() {
+	fmt.Println("starting the task")
+}
 
-	d := task.NewDocker()
-	result := d.Stop(t.ContainerID)
-	if result.Error != nil {
-		log.Printf("Error stopping container %v: %v", d.ContainerId, result.Error)
-	}
-
-	t.FinishTime = time.Now().UTC()
-	t.State = task.Completed
-	w.Db[t.ID] = &t
-	log.Printf("Stopped and removed container %v for task %v", d.ContainerId, t.ID)
-
-	return result
-
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop the taks")
 }
