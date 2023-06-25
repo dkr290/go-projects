fa16af7dad61b1fb4ebfc0b3dbe719aba4b43bc0
d_krasimiro@mail.bg <d_krasimiro@mail.bg>
2023-06-25 17:48:41 +0200

    adding authentication

--- goforum-app/goforum/cmd/app/miggleware.go
+++ goforum-app/goforum/cmd/app/miggleware.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/dkr290/go-projects/goforum-app/goforum/pkg/helpers"
 	"github.com/justinas/nosurf"
 )
 
@@ -35,4 +37,17 @@ func NoSurf(next http.Handler) http.Handler {
 	})
 
 	return nosurfHandler
+}
+
+func Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			app.Session.Put(r.Context(), "error", "you arent logged in")
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			log.Fatal("Error Loggin in")
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
--- goforum-app/goforum/pkg/helpers/helpers.go
+++ goforum-app/goforum/pkg/helpers/helpers.go
@@ -1,9 +1,23 @@
 package helpers
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/dkr290/go-projects/goforum-app/goforum/pkg/config"
+)
+
+var app config.AppConfig
 
 func ErrCheck(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func IsAuthenticated(r *http.Request) bool {
+
+	exists := app.Session.Exists(r.Context(), "user_id")
+
+	return exists
 }
