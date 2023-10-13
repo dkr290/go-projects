525f307eadc59c96136855cc4766eb4cc759d031
dani@emial <dani@emial.com>
2023-10-13 23:43:06 +0200

    adding some devision tests in holidayreservation

--- holidayreservation/main.go
+++ holidayreservation/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
+
+var portNumber = ":8080"
 
 func main() {
 
 	http.HandleFunc("/", HandleHome)
 	http.HandleFunc("/about", HandleAbout)
+	http.HandleFunc("/devide", HandleDevide)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Starting the application on port %s\n", portNumber)
+	if err := http.ListenAndServe(portNumber, nil); err != nil {
+		panic(err)
+	}
 
 }
 
@@ -21,13 +28,27 @@ func HandleAbout(w http.ResponseWriter, r *http.Request) {
 
 	sum := addValues(2, 2)
 
-	fmt.Fprintf(w, "This is the about page of %s\nI like to say %s\nand as a side note, 2 + 2 is %d", owner, saying, sum)
+	_, _ = fmt.Fprintf(w, "This is the about page of %s\nI like to say %s\nand as a side note, 2 + 2 is %d", owner, saying, sum)
 }
 
 // this is the about page
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "This is a home page")
+
+}
+
+func HandleDevide(w http.ResponseWriter, r *http.Request) {
+	x := 100.0
+	y := 0.0
+
+	f, err := devideValues(x, y)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "Error: Division by 0 is not valid operation. Error %s", err)
+		return
+	}
+
+	_, _ = fmt.Fprintf(w, "%f devided by %f is %f", x, y, f)
 
 }
 
@@ -42,4 +63,14 @@ func addValues(x, y int) int {
 
 	return x + y
 
+}
+
+func devideValues(x, y float64) (float64, error) {
+
+	if y == 0 {
+		err := errors.New("devisor is 0")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
 }
