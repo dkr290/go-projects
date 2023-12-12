8c7d0c398253444564b6eb65d0d2e806317f5478
Danail <d_krasimirov@yahoo.com>
2023-12-12 23:43:27 +0100

    adding more tests

--- url/url.go
+++ url/url.go
@@ -1,6 +1,15 @@
 package url
 
-func Parse(url string) error {
-	//return errors.New("mailformed url")
-	return nil
+import "strings"
+
+type URL struct {
+	Scheme string
+}
+
+func Parse(url string) (*URL, error) {
+	i := strings.Index(url, "://")
+	scheme := url[:i]
+
+	return &URL{Scheme: scheme}, nil
+
 }
--- url/url_test.go
+++ url/url_test.go
@@ -4,10 +4,19 @@ import "testing"
 
 func TestParse(t *testing.T) {
 
-	const rawurl = "broken url"
+	const rawurl = "https://foo.com"
+	var u *URL
+	var err error
 
-	if err := Parse(rawurl); err != nil {
+	if u, err = Parse(rawurl); err != nil {
 		t.Logf("Parse %q err = %q, want nil", rawurl, err)
+		t.Fail()
+	}
+	want := "https"
+	got := u.Scheme
+
+	if got != want {
+		t.Logf("Parsing (%q).Scheme = %q; want %q", rawurl, got, want)
 		t.Fail()
 	}
 }
