7f9bf7fc33d6794e8afb43b104094fad9949d028
Danail <d_krasimirov@yahoo.com>
2024-02-04 18:09:05 +0100

    adding small project

---
+++ accountbalance/communication.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func presentOptions() {
+	fmt.Println("Wellcome to the go Bank!")
+	fmt.Println("What do you want to do!")
+	fmt.Println("1. Check Ballance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+}
