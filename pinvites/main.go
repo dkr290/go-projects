253433d6a6aef7322bff181abca511233a984806
Danail <d_krasimirov@yahoo.com>
2024-05-19 22:50:14 +0200

    adding the verification

--- pinvites/main.go
+++ pinvites/main.go
@@ -8,7 +8,7 @@ import (
 
 type Rsvp struct {
 	Name, Email, Phone string
-	WillATtend         bool
+	Willattend         bool
 }
 type formData struct {
 	*Rsvp
@@ -47,14 +47,47 @@ func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 	templ["welcome"].Execute(w, nil)
 }
 func listHandler(w http.ResponseWriter, r *http.Request) {
-	templ["list"].Execute(w, nil)
+	templ["list"].Execute(w, responses)
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		templ["form"].Execute(w, formData{
-			Rsvp:   &Rsvp{},
+			Rsvp: &Rsvp{
+				Willattend: false,
+			},
 			Errors: []string{},
 		})
+	} else if r.Method == http.MethodPost {
+		r.ParseForm()
+		respData := Rsvp{
+			Name:       r.Form["name"][0],
+			Email:      r.Form["email"][0],
+			Phone:      r.Form["phone"][0],
+			Willattend: r.Form["willattend"][0] == "true",
+		}
+		errors := []string{}
+		if respData.Name == "" {
+			errors = append(errors, "Please enter your name")
+		}
+		if respData.Email == "" {
+			errors = append(errors, "Please enter your email address")
+		}
+		if respData.Phone == "" {
+			errors = append(errors, "Please enter your phone number")
+		}
+		if len(errors) > 0 {
+			templ["form"].Execute(w, formData{
+				Rsvp: &respData, Errors: errors,
+			})
+		} else {
+
+			responses = append(responses, &respData)
+			if respData.Willattend {
+				templ["thanks"].Execute(w, respData.Name)
+			} else {
+				templ["sorry"].Execute(w, respData.Name)
+			}
+		}
 	}
 }
