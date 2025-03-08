7e339687e72a4f19762e733efc470315a2f30e70
Danail <d_krasimirov@yahoo.com>
2025-03-08 23:21:32 +0100

    improving the version of embedding

--- llm/pdf-extraxt/go.mod
+++ llm/pdf-extraxt/go.mod
@@ -2,7 +2,10 @@ module pdf-extract
 
 go 1.23.0
 
-require github.com/tmc/langchaingo v0.1.13
+require (
+	github.com/qdrant/go-client v1.13.0
+	github.com/tmc/langchaingo v0.1.13
+)
 
 require (
 	github.com/dlclark/regexp2 v1.10.0 // indirect
@@ -14,5 +17,10 @@ require (
 	gitlab.com/golang-commonmark/markdown v0.0.0-20211110145824-bf3e522c626a // indirect
 	gitlab.com/golang-commonmark/mdurl v0.0.0-20191124015652-932350d1cb84 // indirect
 	gitlab.com/golang-commonmark/puny v0.0.0-20191124015043-9f83538fa04f // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240827150818-7e3bb234dfed // indirect
+	google.golang.org/grpc v1.66.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
--- llm/pdf-extraxt/main.go
+++ llm/pdf-extraxt/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"pdf-extract/pkg/pdf"
+
+	"github.com/qdrant/go-client/qdrant"
 )
 
 func main() {
@@ -10,11 +13,13 @@ func main() {
 	p := pdf.New("./BOI.pdf")
 	if err := p.ReadPdf(); err != nil {
 		slog.Error("error reading pdf ", "error", err)
+		return
 	}
 	// split text in chunks
 	docs, err := p.SplitText()
 	if err != nil {
 		slog.Error("error splitting the text", "error", err)
+		return
 	}
 
 	// for _, doc := range docs {
@@ -25,16 +30,60 @@ func main() {
 	mdTextChunks, err := p.AddMetadata(docs, "BOI US FinCEN")
 	if err != nil {
 		slog.Error("error metadata ", "error", err)
+		return
 	}
+
+	// err = createCollection("vector_store1", "qdrant.172.22.0.5.nip.io/grpc")
+	// if err != nil {
+	// 	slog.Error("error creating collection", "error", err)
+	// 	return
+	// }
 
 	err = p.GenEmbeddings(
 		mdTextChunks,
 		"nomic-embed-text",
 		"http://172.22.0.3/ollama",
-		"http://qdrant.172.22.0.4.nip.io",
+		"http://qdrant.172.22.0.5.nip.io",
 		"vector_store1",
 	)
 	if err != nil {
 		slog.Error("error adding the embeddings", "error", err)
 	}
 }
+
+func uint32Ptr(i uint32) *uint32 { return &i }
+
+func createCollection(coll string, host string) error {
+	client, err := qdrant.NewClient(&qdrant.Config{
+		Host:   host,
+		Port:   80,
+		UseTLS: false,
+	})
+	if err != nil {
+		return err
+	}
+	err = client.CreateCollection(context.Background(), &qdrant.CreateCollection{
+		CollectionName:    coll,
+		ShardNumber:       uint32Ptr(6),
+		ReplicationFactor: uint32Ptr(3),
+		VectorsConfig: qdrant.NewVectorsConfig(&qdrant.VectorParams{
+			Size:     768,
+			Distance: qdrant.Distance_Cosine,
+		}),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// curl -X PUT "http://localhost:6333/collections/my_collection" \
+//      -H "Content-Type: application/json" \
+//      -d '{
+//            "vectors": {
+//                "size": 128,
+//                "distance": "Cosine"
+//            },
+//            "shard_number": 6,
+//            "replication_factor": 3
+//          }'
