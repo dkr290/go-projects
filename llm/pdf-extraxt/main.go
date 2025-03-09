79627e237e5ee4f95829fae933b073383eaa1eb8
Danail <d_krasimirov@yahoo.com>
2025-03-09 21:31:07 +0100

    finish PDF exctract now

--- llm/pdf-extraxt/main.go
+++ llm/pdf-extraxt/main.go
@@ -2,79 +2,88 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log/slog"
+	"pdf-extract/pkg/collection"
 	"pdf-extract/pkg/pdf"
+	"pdf-extract/pkg/response"
+	"time"
+)
 
-	"github.com/qdrant/go-client/qdrant"
+var (
+	initVectors bool
+
+	userQuery        string
+	chatModel        = "llama3.2"
+	embedModel       = "nomic-embed-text"
+	ollamaUrl        = "http://172.22.0.5/ollama"
+	qUrl             = "http://qdrant.172.22.0.5.nip.io:6333"
+	vectorCollection = "vector_store1"
 )
 
 func main() {
+	flag.BoolVar(&initVectors, "init", false, "init set to truth is to init the vectgor embedding")
+	flag.StringVar(&userQuery, "query", "", "asking question")
+
+	flag.Parse()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Hour)
+	defer cancel()
+	if initVectors {
+		embed_run()
+	}
+
+	if userQuery != "" {
+		r := response.New(chatModel, embedModel, ollamaUrl, qUrl, vectorCollection, userQuery, ctx)
+		if err := r.QuestionResponse(); err != nil {
+			slog.Error("unexpected error of question responce", "error", err)
+		}
+
+	}
+}
+
+func embed_run() {
 	// new PDF reading
 	p := pdf.New("./BOI.pdf")
 	if err := p.ReadPdf(); err != nil {
 		slog.Error("error reading pdf ", "error", err)
 		return
 	}
+	fmt.Println("Reading the pdf")
 	// split text in chunks
 	docs, err := p.SplitText()
 	if err != nil {
 		slog.Error("error splitting the text", "error", err)
 		return
 	}
+	fmt.Println("Splitting the documents to chunks")
 
-	// for _, doc := range docs {
-	// 	fmt.Println("")
-	// 	log.Println(doc)
-	// }
 	// add metadata to the chunks
 	mdTextChunks, err := p.AddMetadata(docs, "BOI US FinCEN")
 	if err != nil {
 		slog.Error("error metadata ", "error", err)
 		return
 	}
+	fmt.Println("Adding metadata to the chunks")
 
-	// err = createCollection("vector_store1", "qdrant.172.22.0.5.nip.io/grpc")
-	// if err != nil {
-	// 	slog.Error("error creating collection", "error", err)
-	// 	return
-	// }
+	err = collection.CreateCollection("vector_store1", "qdrant-grpc.172.22.0.5.nip.io")
+	if err != nil {
+		slog.Error("error creating collection", "error", err)
+		return
+	}
+	fmt.Println("Creating the qdrant collection")
 
 	err = p.GenEmbeddings(
 		mdTextChunks,
 		"nomic-embed-text",
-		"http://172.22.0.3/ollama",
-		"http://qdrant.172.22.0.5.nip.io",
+		"http://172.22.0.5/ollama",
+		"http://qdrant.172.22.0.5.nip.io:6333",
 		"vector_store1",
 	)
 	if err != nil {
 		slog.Error("error adding the embeddings", "error", err)
 	}
-}
-
-func uint32Ptr(i uint32) *uint32 { return &i }
-
-func createCollection(coll string, host string) error {
-	client, err := qdrant.NewClient(&qdrant.Config{
-		Host:   host,
-		Port:   80,
-		UseTLS: false,
-	})
-	if err != nil {
-		return err
-	}
-	err = client.CreateCollection(context.Background(), &qdrant.CreateCollection{
-		CollectionName:    coll,
-		ShardNumber:       uint32Ptr(6),
-		ReplicationFactor: uint32Ptr(3),
-		VectorsConfig: qdrant.NewVectorsConfig(&qdrant.VectorParams{
-			Size:     768,
-			Distance: qdrant.Distance_Cosine,
-		}),
-	})
-	if err != nil {
-		return err
-	}
-	return nil
+	fmt.Println("Finish with vector embedding")
 }
 
 // curl -X PUT "http://localhost:6333/collections/my_collection" \
